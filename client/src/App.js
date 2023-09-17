52181d858f47f16efb8591a6962bbe1b3d0d8e57
Theo0259 <122873258+Theo0259@users.noreply.github.com>
2023-09-17 23:38:21 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js";
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={About} />
+        <Route path="/skills" element={Skills} />
+        <Route path="/projects" element={Projects} />
+        <Route path="/contact" element={Contact} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
